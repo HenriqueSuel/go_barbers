c3c9b1d45e1e761ff3be00a3aa12c02e9206f85d
Henrique <h.suel17@hotmail.com>
2020-07-11 19:58:37 -0300

    Enviando notificacoes

--- src/modules/appointments/services/CreatedAppointmentService.ts
+++ src/modules/appointments/services/CreatedAppointmentService.ts
@@ -1,10 +1,12 @@
 import "reflect-metadata"
-import Appointments from '@modules/appointments/infra/typeorm/entities/Appointments'
-import AppError from '@shared/errors/AppError'
-import IAppointmentsRepository from '../repositories/IAppointmentsRepository'
-import { startOfHour, isBefore, getHours } from 'date-fns';
+import { startOfHour, isBefore, getHours, format } from 'date-fns';
 import { injectable, inject } from 'tsyringe';
 
+import AppError from '@shared/errors/AppError';
+
+import Appointments from '@modules/appointments/infra/typeorm/entities/Appointments'
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository'
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository'
 
 interface IRequest {
   provider_id: string,
@@ -17,7 +19,10 @@ class CreatedAppointmentService {
 
   constructor(
     @inject('AppointmentsRepository')
-    private appointmentsRepository: IAppointmentsRepository
+    private appointmentsRepository: IAppointmentsRepository,
+
+    @inject('NotificationsRepository')
+    private notificationsRepository: INotificationsRepository
   ) { }
 
   public async execute({ provider_id, date, user_id }: IRequest): Promise<Appointments> {
@@ -27,7 +32,6 @@ class CreatedAppointmentService {
       throw new AppError("You can't create an appointemnt with yourself", 400)
     }
 
-    console.log(getHours(appointmentDate), date)
     if (getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17) {
       throw new AppError(
         'You can only create appointments between 8am and 5pm',
@@ -45,6 +49,13 @@ class CreatedAppointmentService {
     }
 
     const appointment = await this.appointmentsRepository.create({ provider_id, date: appointmentDate, user_id })
+
+    const dateFormatted = format(appointmentDate, "dd/MM/yyyy 'às' HH:mm'h'");
+
+    await this.notificationsRepository.create({
+      recipient_id: provider_id,
+      content: `Novo agendamento para dia ${dateFormatted}`
+    })
 
     return appointment;
   }
